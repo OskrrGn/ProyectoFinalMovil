6d0bfe542ecb4fdcb9f623516b833ec69702f909
OskrrGn <155512833+OskrrGn@users.noreply.github.com>
2025-06-23 19:04:15 -0600

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsAppTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val viewModel = NewsViewModel()
+                    NewsListScreen(viewModel)
+                }
+            }
+        }
+    }
+}
