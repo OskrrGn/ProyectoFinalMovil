182d7b7eb3a78debb099a53e838d583b9c2a3928
OskrrGn <155512833+OskrrGn@users.noreply.github.com>
2025-06-23 18:54:41 -0600

    Create noticias.sql

---
+++ noticias.sql
@@ -0,0 +1,6 @@
+CREATE TABLE noticias (
+  id int(11) NOT NULL,
+  titulo varchar(255) NOT NULL,
+  contenido text NOT NULL,
+  fecha date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
